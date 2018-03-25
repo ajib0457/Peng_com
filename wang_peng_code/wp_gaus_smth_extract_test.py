167065357e9dae30f79246d271103844ee436468
Simulations analysis <35188545+ajib0457@users.noreply.github.com>
2018-03-25 17:39:21 +1100

    Major changes

--- wang_peng_code/wp_gaus_smth_extract_test.py
+++ wang_peng_code/wp_gaus_smth_extract_test.py
@@ -8,9 +8,6 @@ from scipy import ndimage
 sim_sz=60           #Size of simulation in physical units Mpc/h cubed
 grid_nodes=128      #Density Field grid resolution
 smooth_scl=2.0      #Smoothing scale in physical units Mpc/h
-tot_mass_bins=4     #Number of Halo mass bins
-particles_filt=300  #Halos to filter out based on number of particles, ONLY for Dot Product Spin-LSS(SECTION 5.)
-Mass_res=1.35*10**8 #Bolchoi particle mass as per, https://arxiv.org/pdf/1002.3660.pdf
 
 #Load Bolchoi Simulation Catalogue, ONLY filtered for X,Y,Z=<sim_sz
 f=h5py.File("bolchoi_DTFE_rockstar_box_%scubed_xyz_vxyz_jxyz_m_r.h5"%sim_sz, 'r')
@@ -54,6 +51,20 @@ s=1.0*smooth_scl/sim_sz*grid_nodes# s- standard deviation of Kernel, converted f
 #smooth via ndimage function
 img = ndimage.gaussian_filter(image_orgnl,s,order=0,mode='wrap',truncate=20)#smoothing function
 
+#3d smoothing via convolution method
+in_val,fnl_val=-grid_nodes/2.0,grid_nodes/2.0#kernel values
+X,Y,Z=np.meshgrid(np.linspace(in_val,fnl_val,grid_nodes),np.linspace(in_val,fnl_val,grid_nodes),np.linspace(in_val,fnl_val,grid_nodes))
+h=(1/np.sqrt(1.0*2*np.pi*s*s))**(3)*np.exp(-1/(1.0*2*s*s)*((Y-0.5)**2+(X-0.5)**2+(Z-0.5)**2))
+#h=(1/sqrt(1.0*2*pi*s*s))**(3)*exp(-1/(1.0*2*s*s)*((Y-0.5)**2+(X-0.5)**2+(Z-0.5)**2))
+area=np.sum(h)
+h=h/area
+h=np.roll(h,int(grid_nodes/2),axis=0)
+h=np.roll(h,int(grid_nodes/2),axis=1)
+h=np.roll(h,int(grid_nodes/2),axis=2)
+fft_dxx=np.fft.fftn(h)
+fft_db=np.fft.fftn(image_orgnl)
+ifft_dxx=np.fft.ifftn(np.multiply(fft_dxx,fft_db)).real
+
 #-----------------------
 # Gaussian smooth PENG METHOD
 #-----------------------
@@ -88,28 +99,53 @@ for i in range(grid_nodes):
 smoothed_image = np.fft.ifftn(image).real
 #---------------------------------
 
-'''
-hf = h5py.File('CIC_density_smoothed.h5', 'r')
-smoothed_image_peng=hf['CIC_density_smoothed'][:]
-hf.close()
-smoothed_image_peng=np.reshape(smoothed_image_peng,(grid_nodes,grid_nodes,grid_nodes))
-'''
+#calculate difference between smoothed fields
+my_mns_peng=np.sum(abs(ifft_dxx.flatten()-smoothed_image.flatten()))
+my_mns_ndimag=np.sum(abs(ifft_dxx.flatten()-img.flatten()))
+
+ndimag_mns_peng=np.sum(abs(img.flatten()-smoothed_image.flatten()))
+ndimag_mns_my=np.sum(abs(img.flatten()-ifft_dxx.flatten()))
+
+peng_mns_my=np.sum(abs(smoothed_image.flatten()-ifft_dxx.flatten()))
+peng_mns_ndimag=np.sum(abs(smoothed_image.flatten()-img.flatten()))
+
 #plotting
-slc=30
 plt.figure(figsize=(15,17))
+#Plotting global features
+slc=15
+
+columns=['Density Values']
+subtitl_offset=1.15
+ttl_fnt=12
+plt.subplots_adjust(hspace=0.3,wspace=0.12,top=0.85)
+plt.suptitle("Density fld smoothing comparison.\nSmoothing scale: %s pxls \nSlice: %s/%s"%(round(s,4),slc,grid_nodes),y=0.95,fontsize=20)
 col=2
 ro=2
+
 ax1=plt.subplot2grid((col,ro), (0,1)) 
-plt.title('ndimage')
+plt.title('ndimage func',y=subtitl_offset,fontsize=ttl_fnt)
 cmapp = plt.get_cmap('jet')
 scl_plt=5#reduce scale of density fields and eigenvalue subplots by increasing number
 dn_fl_plt=ax1.imshow(np.power(img[slc,:,:],1.0/scl_plt),cmap=cmapp,extent=[0,grid_nodes,0,grid_nodes])#The colorbar will adapt to data
 plt.colorbar(dn_fl_plt,cmap=cmapp)
+#generate sub-table
+data_fnc=np.round(np.array([[ndimag_mns_peng],[ndimag_mns_my]]),8)
+rows=['ndimag - peng','ndimag - my']
+tbl_diff=plt.table(cellText=data_fnc,loc='top',rowLabels=rows,colLabels=columns,colWidths=[0.5 for x in columns],cellLoc='center')
+tbl_diff.set_fontsize(12)
+tbl_diff.scale(1.2, 1.2)
+
 ax2=plt.subplot2grid((col,ro), (1,0)) 
-plt.title('Peng')
+plt.title('Peng method',y=subtitl_offset,fontsize=ttl_fnt)
 scl_plt=5#reduce scale of density fields and eigenvalue subplots by increasing number
 dn_fl_plt=ax2.imshow(np.power(smoothed_image[slc,:,:],1.0/scl_plt),cmap=cmapp,extent=[0,grid_nodes,0,grid_nodes])#The colorbar will adapt to data
 plt.colorbar(dn_fl_plt,cmap=cmapp)
+#generate sub-table
+data_fnc=np.round(np.array([[peng_mns_my],[peng_mns_ndimag]]),8)
+rows=['peng - my','peng - ndimag']
+tbl_diff=plt.table(cellText=data_fnc,loc='top',rowLabels=rows,colLabels=columns,colWidths=[0.5 for x in columns],cellLoc='center')
+tbl_diff.set_fontsize(12)
+tbl_diff.scale(1.2, 1.2)
 
 ax3=plt.subplot2grid((col,ro), (0,0)) 
 plt.title('density field')
@@ -117,7 +153,18 @@ scl_plt=5#reduce scale of density fields and eigenvalue subplots by increasing n
 dn_fl_plt=ax3.imshow(np.power(image_orgnl[slc,:,:],1.0/scl_plt),cmap=cmapp,extent=[0,grid_nodes,0,grid_nodes])#The colorbar will adapt to data
 plt.colorbar(dn_fl_plt,cmap=cmapp)
 
+ax3=plt.subplot2grid((col,ro), (1,1)) 
+plt.title('convolution method',y=subtitl_offset,fontsize=ttl_fnt)
+scl_plt=5#reduce scale of density fields and eigenvalue subplots by increasing number
+dn_fl_plt=ax3.imshow(np.power(ifft_dxx[slc,:,:],1.0/scl_plt),cmap=cmapp,extent=[0,grid_nodes,0,grid_nodes])#The colorbar will adapt to data
+plt.colorbar(dn_fl_plt,cmap=cmapp)
+#generate sub-table
+data_fnc=np.round(np.array([[my_mns_peng],[my_mns_ndimag]]),8)
+rows=['my - peng','my - ndimag']
+tbl_diff=plt.table(cellText=data_fnc,loc='top',rowLabels=rows,colLabels=columns,colWidths=[0.5 for x in columns],cellLoc='center')
+tbl_diff.set_fontsize(12)
+tbl_diff.scale(1.2, 1.2)
 
 
-
+plt.savefig('/import/oth3/ajib0457/wang_peng_code/Peng_run_bolchoi_smpl_python/smoothed_comp_plt/bolchoi_smpl%sMpch_smth%sMpch_grid%s.png'%(sim_sz,smooth_scl,grid_nodes))
 
